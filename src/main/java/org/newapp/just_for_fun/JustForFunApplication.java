3b605281fcb804ba21655b61666fd53c3e4431cd
ahmed dacharoui <dachraouia193@gmail.com>
2024-08-16 23:30:28 +0100

    first commit

---
+++ src/main/java/org/newapp/just_for_fun/JustForFunApplication.java
@@ -0,0 +1,13 @@
+package org.newapp.just_for_fun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustForFunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustForFunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/newapp/just_for_fun/JustForFunApplicationTests.java
@@ -0,0 +1,13 @@
+package org.newapp.just_for_fun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustForFunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
