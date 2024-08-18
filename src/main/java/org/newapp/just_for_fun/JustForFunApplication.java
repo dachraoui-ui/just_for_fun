09ff645c336ca1cd4edb4162e55b21928db9196a
ahmed dacharoui <dachraouia193@gmail.com>
2024-08-18 21:41:22 +0100

    Change the code to work with postgre database

--- src/main/java/org/newapp/just_for_fun/JustForFunApplication.java
+++ src/main/java/org/newapp/just_for_fun/JustForFunApplication.java
@@ -1,22 +1,26 @@
 package org.newapp.just_for_fun;
 
 
-import org.newapp.just_for_fun.config.Pizza;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class JustForFunApplication implements CommandLineRunner {
 
-    private Pizza pizza;
 
-   public JustForFunApplication(Pizza pizza){
-       this.pizza = pizza;
-   }
+    private DataSource dataSource;
+
+    public JustForFunApplication(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
     private static final Logger log = LoggerFactory.getLogger(JustForFunApplication.class);
 
     public static void main(String[] args) {
@@ -25,12 +29,9 @@ public class JustForFunApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        log.info(
-                String.format("I Want a %s crust pizza, with %s and %s sauce ",
-                        pizza.getCrust(),
-                        pizza.getTopping(),
-                pizza.getSauce())
-        );
+        log.info("DataSource {}", dataSource.toString());
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.execute("select 1");
     }
 }
 
