e30145a03b11c092fc0b4954f667c71ebaaaad8c
ahmed dacharoui <dachraouia193@gmail.com>
2024-08-19 18:51:08 +0100

    add author and book classes

---
+++ src/main/java/org/newapp/just_for_fun/domain/Author.java
@@ -0,0 +1,12 @@
+package org.newapp.just_for_fun.domain;
+
+import lombok.Data;
+
+@Data
+public class Author {
+    private int id;
+
+    private String name;
+
+    private int age;
+}
---
+++ src/main/java/org/newapp/just_for_fun/domain/Book.java
@@ -0,0 +1,11 @@
+package org.newapp.just_for_fun.domain;
+
+import lombok.Data;
+
+@Data
+public class Book {
+
+    private String isbn;
+
+    private String title;
+}
