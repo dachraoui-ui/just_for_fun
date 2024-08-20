87a52f00df3f5ca38926f5056faf634e3e28ab95
ahmed dacharoui <dachraouia193@gmail.com>
2024-08-20 19:23:57 +0100

    add test app

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,5 @@
+create sequence if not exists authors_id_seq;
+
 drop table if exists "books";
 drop table if exists "authors";
 
@@ -5,13 +7,14 @@ create table "authors"(
     "id" bigint default nextval('authors_id_seq') not null,
     "name" text,
     "age" integer,
-    constraint "authors_pkey" primary key ("id");
+    constraint "authors_pkey" primary key ("id")
 );
+
 create table "books"(
-"isbn" text not null,
-"title" text,
-"author_id" bigint,
-constraint "books_pkey" primary key ("isbn"),
-constraint "fk_author" foreign key(author_id)
-references authors(id)
+    "isbn" text not null,
+    "title" text,
+    "author_id" bigint,
+    constraint "books_pkey" primary key ("isbn"),
+    constraint "fk_author" foreign key(author_id)
+    references authors(id)
 );
